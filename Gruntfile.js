a70a6712a891942437ec90121cf5a3085f0a4904
Eric King <eric@webdeveric.com>
2015-02-21 23:32:49 -0600

    Updated Grunt workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,183 +1,170 @@
-module.exports = function( grunt ){
-    'use strict';
+module.exports = function( grunt ) {
+    "use strict";
 
-    var config = {
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            files: ['Gruntfile.js', 'js/src/**/*.js', '!js/src/**/*.min.js', '!js/src/modernizr.js'],
-            options: {
-                globals: {
-                    jQuery: true,
-                    console: true,
-                    module: true,
-                    document: true
-                }
-            }
-        },
-        uglify: {
-            build: {
+    var jsFiles = [ "Gruntfile.js", "./assets/js/**/*.js", "!./assets/js/**/*.min.js" ],
+        config = {
+
+            pkg: grunt.file.readJSON("package.json"),
+
+            jshint: {
+                files: jsFiles,
                 options: {
-                    banner: '/* <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */',
-                    sourceMap: true, //'js/dist/<%= pkg.name %>.js',
-                    // mangle: false,
-                    preserveComments: 'some',
-                    compress: {
-                        // drop_console: true
+                    jshintrc: "./.jshintrc",
+                    globals: {
+                        jQuery: true,
+                        console: true,
+                        module: true,
+                        document: true
                     }
-                },
-                files: [ {
-                    expand: true,
-                    cwd: 'js/src/',
-                    src: '**/*.js',
-                    dest: 'js/dist',
-                    ext: '.min.js'
-                } ]
-            }
-        },
-
-        compass: {
-            css: {
-                options: {
-                    sassDir: 'scss',
-                    cssDir: 'css/src'
                 }
-            }
-        },
-
-        sass: {
-            dist: {
-                files: [ {
-                    expand: true,
-                    cwd: 'scss',
-                    src: ['*.scss'],
-                    dest: 'css/src',
-                    ext: '.css'
-                } ]
-            }
-        },
-
-        cssmin: {
-            options: {
-                banner: '/* <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */',
             },
-            minify:{
-                expand: true,
-                cwd: 'css/src/',
-                src: ['*.css', '!*.min.css'],
-                dest: 'css/dist/',
-                ext: '.min.css'
-            }
-        },
 
-        imagemin: {
-            png: {
+            jscs: {
+                src: jsFiles,
                 options: {
-                    optimizationLevel: 7
-                },
-                files: [ {
-                    expand: true,
-                    cwd: 'imgs/src/',
-                    src: ['**/*.png'],
-                    dest: 'imgs/dist/',
-                    ext: '.png'
-                } ]
+                    config: "./.jscs.json"
+                }
             },
-            jpg: {
-                options: {
-                    progressive: true
-                },
-                files: [ {
-                    expand: true,
-                    cwd: 'imgs/src/',
-                    src: ['**/*.jpg'],
-                    dest: 'imgs/dist/',
-                    ext: '.jpg'
-                } ]
+
+            plato: {
+                app: {
+                    options: {
+                        jshint: grunt.file.readJSON("./.jshintrc")
+                    },
+                    files: {
+                        "reports/plato": jsFiles
+                    }
+                }
             },
-            gif: {
-                files: [ {
-                    expand: true,
-                    cwd: 'imgs/src/',
-                    src: ['**/*.gif'],
-                    dest: 'imgs/dist/',
-                    ext: '.gif'
-                } ]
-            }
-        },
-		checkwpversion: {
-		    options:{
-		        readme: 'readme.txt',
-		        plugin: '<%= pkg.name %>.php',
-		    },
-		    check: {
-		        version1: 'plugin',
-		        version2: 'readme',
-		        compare: '=='
-		    },
-		    check2: {
-		        version1: 'plugin',
-		        version2: '<%= pkg.version %>',
-		        compare: '==',
-		    }
-		},
-        watch: {
-        	checkwpversion: {
-				files: ['readme.txt', 'package.json', '<%= pkg.name %>.php'],
-				tasks: ['checkwpversion']
-        	},
+
+            uglify: {
+                build: {
+                    options: {
+                        // banner: "/* <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today('yyyy-mm-dd') %> */",
+                        sourceMap: true,
+                        // mangle: false,
+                        preserveComments: "some",
+                        compress: {
+                            drop_console: true
+                        }
+                    },
+                    files: [ {
+                        expand: true,
+                        cwd: "./assets/js/",
+                        src: [ "**/*.js", "!**/*.min.js" ],
+                        dest: "./assets/js/",
+                        ext: ".min.js"
+                    } ]
+                }
+            },
+
             sass: {
-                files: ['scss/**/*.{scss,sass}'],
-                tasks: ['compass', 'cssmin']
-            },
-            js: {
-                files: '<%= jshint.files %>',
-                tasks: ['jshint', 'uglify']
-            },
-            pngimgs: {
-                files: ['imgs/src/**/*.png'],
-                tasks: ['imagemin:png'],
-                options: {
-                    spawn: false
+                css: {
+                    options: {
+                        style: "compressed"
+                    },
+                    files: [ {
+                        expand: true,
+                        cwd: "./assets/scss/",
+                        src: [ "*.scss", "*.sass" ],
+                        dest: "./assets/css/",
+                        ext: ".css"
+                    } ]
                 }
             },
-            jpgimgs: {
-                files: ['imgs/src/**/*.jpg'],
-                tasks: ['imagemin:jpg'],
+
+            autoprefixer: {
                 options: {
-                    spawn: false
-                }
-            },
-            gifimgs: {
-                files: ['imgs/src/**/*.gif'],
-                tasks: ['imagemin:gif'],
-                options: {
-                    spawn: false
-                }
-            },
-            livereload: {
-                options: {
-                    livereload: true
+                    browsers: [ "last 3 versions", "ie 8", "ie 9" ],
+                    map: true
                 },
-                files: [
-                    'imgs/dist/**/*',
-                    'css/dist/**/*',
-                    'js/dist/**/*',
-                ]
+                css: {
+                    expand: true,
+                    flatten: true,
+                    src: "./assets/css/*.css",
+                    dest: "./assets/css/"
+                },
+            },
+
+            checkwpversion: {
+                options:{
+                    readme: "readme.txt",
+                    plugin: "<%= pkg.name %>.php",
+                },
+                check: {
+                    version1: "plugin",
+                    version2: "readme",
+                    compare: "=="
+                },
+                check2: {
+                    version1: "plugin",
+                    version2: "<%= pkg.version %>",
+                    compare: "==",
+                }
+            },
+
+            watch: {
+                checkwpversion: {
+                    files: [ "readme.txt", "package.json", "<%= pkg.name %>.php" ],
+                    tasks: [ "checkwpversion" ]
+                },
+                sass: {
+                    files: [ "./assets/scss/**/*.{scss,sass}" ],
+                    tasks: [ "style" ]
+                },
+                js: {
+                    files: jsFiles,
+                    tasks: [ "js" ]
+                },
+                livereload: {
+                    options: {
+                        livereload: true
+                    },
+                    files: [
+                        "./assets/css/**/*",
+                        "./assets/js/dist/**/*",
+                    ]
+                }
             }
-        }
-    };
+        };
 
-    grunt.initConfig( config );
+    grunt.config.init( config );
 
-    grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
-    grunt.loadNpmTasks( 'grunt-contrib-sass' );
-    grunt.loadNpmTasks( 'grunt-contrib-compass' );
-    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-    grunt.loadNpmTasks( 'grunt-checkwpversion' );
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    // https://github.com/sindresorhus/load-grunt-tasks
+    require("load-grunt-tasks")(grunt);
 
-    grunt.registerTask( 'test', [ 'checkwpversion', 'jshint' ] );
-    grunt.registerTask( 'default', [ 'checkwpversion', 'compass', 'jshint', 'uglify', 'cssmin' ] );
+    // https://www.npmjs.com/package/time-grunt
+    require("time-grunt")(grunt);
+
+    grunt.registerTask(
+        "default",
+        [ "checkwpversion", "assets", "watch" ]
+    );
+
+    grunt.registerTask(
+        "check",
+        [ "checkwpversion", "jshint", "jscs" ]
+    );
+
+    grunt.registerTask(
+        "assets",
+        [ "style", "js" ]
+    );
+
+    grunt.registerTask(
+        "style",
+        [ "sass", "autoprefixer" ]
+    );
+
+    grunt.registerTask(
+        "js",
+        [ "jshint", "jscs", "uglify" ]
+    );
+
+    grunt.registerTask(
+        "reports",
+        [ "jshint", "jscs", "plato" ]
+        /* , "shell:platoreports" */
+    );
+
 };
